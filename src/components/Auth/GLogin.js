61cb6a21bf7930c818c52523243edd755f888b41
Phuc Nguyen Dang [4]VN.Danang <phuc.nguyen4@monstar-lab.com>
2022-08-02 09:04:58 +0700

    initial template

---
+++ src/components/Auth/GLogin.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import useHttpClient from "../../hooks/useHttpClient";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+
+const GLogin = (props) => {
+  const [showLoginButton, setShowLoginButton] = useState(true);
+  const { setError } = useHttpClient();
+
+  // Configure Firebase.
+  const config = {
+    apiKey: "AIzaSyC_t25GAyQ1m8BDvgCMu0dGKXnQFCjanw8",
+    authDomain: "nodejs-community.firebaseapp.com",
+    // ...
+  };
+
+  firebase.initializeApp(config);
+
+  useEffect(() => {
+    const unregisterAuthObserver = firebase
+      .auth()
+      .onAuthStateChanged(async (user) => {
+        if (user) {
+          console.log(user, "user in GLogin");
+          props.onLogin(user);
+          setShowLoginButton(false);
+        } else {
+          setError("Login with Google failed. Please try again!", user);
+        }
+      });
+    return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
+  }, []);
+
+  // Configure FirebaseUI.
+  const uiConfig = {
+    // Popup signin flow rather than redirect flow.
+    signInFlow: "popup",
+    // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+    signInSuccessUrl: "/",
+    // We will display Google and Facebook as auth providers.
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    ],
+  };
+  return (
+    <div className="auth__google">
+      {(showLoginButton && (<StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />))}
+    </div>
+  );
+};
+
+export default GLogin;
