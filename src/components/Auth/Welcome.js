5f3645217f2027700db47877b7059d2f84c759d5
Phuc Nguyen Dang [4]VN.Danang <phuc.nguyen4@monstar-lab.com>
2022-08-04 15:18:48 +0700

    custom page login

--- src/components/Auth/Welcome.js
+++ src/components/Auth/Welcome.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 const Welcome = () => {
   return (
     <div className="welcome">
-      <h2 className="welcome__title">Welcome to DEV Community</h2>
+      <h2 className="welcome__title">Welcome to FatFish Community</h2>
       <p className="welcome__slogan">
-        <Link to="/">DEV Community</Link> is a community of 748,239 amazing
-        developers
+        <Link to="/">FatFish</Link> is a community of amazing NodeJS developer
+        at Monstar-lab
       </p>
     </div>
   )
