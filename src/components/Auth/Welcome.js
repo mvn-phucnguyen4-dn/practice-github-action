45e8e3de8674468b8cda5d590eb4da33f598b808
Phuc Nguyen Dang [4]VN.Danang <phuc.nguyen4@monstar-lab.com>
2022-08-05 22:08:07 +0700

    fix ui login

--- src/components/Auth/Welcome.js
+++ src/components/Auth/Welcome.js
@@ -5,9 +5,6 @@ const Welcome = () => {
   return (
     <div className="welcome">
       <h2 className="welcome__title">Welcome to FatFish Community</h2>
-      <p className="welcome__slogan">
-        <Link to="/">FatFish</Link> is a community of amazing NodeJS developer
-      </p>
     </div>
   )
 }
